2c63d6b70c1b900b1ae5c3ad669c9f5398e75d08
月杪 <moonkinstar@gmail.com>
2016-06-12 16:43:41 +0800

    init

---
+++ MagicOcean/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  MagicOcean
+//
+//  Created by Wu Hengmin on 16/6/8.
+//  Copyright © 2016年 Wu Hengmin. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        weak var weakSelf = self
+//        let headers = [
+//            "Authorization": "Bearer "+Personal_Access_Tokens,
+//            "Content-Type": "application/json"
+//        ]
+//        
+//        Alamofire.request(.GET, BASE_URL+URL_ACCOUNT, parameters: nil, encoding: .URL, headers: headers).responseJSON { response in
+//            if let strongSelf = weakSelf {
+//                let str = response.result.value as! NSDictionary
+//                print(str)
+//            }
+//        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func pressed(sender: AnyObject) {
+        let authPath:String = "https://cloud.digitalocean.com/v1/oauth/authorize?response_type=code&client_id=\(ClientID)&redirect_uri=\(redirect_uri)&scope=read write&state=0807edf7d85e5d"
+        print(authPath)
+        let escapedAddress:String = authPath.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+//        authPath = authPath.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+//        print(authPath)
+        if let authURL:NSURL = NSURL(string: escapedAddress) {
+            if UIApplication.sharedApplication().openURL(authURL) {
+                print(2)
+            } else {
+                print(1)
+            }
+        }
+        
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
+# MagicOcean
+=======
 # MagicOcean
 DigitalOcean第三方客户端
+>>>>>>> 9d27b2c3bfd5402e02185eabdd500fefd239e0b1
