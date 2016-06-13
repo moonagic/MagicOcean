4313b5c624cd9d4e47cf8607d8d16c1b1968e0f6
月杪 <moonkinstar@gmail.com>
2016-06-13 13:43:50 +0800

    update README.md

---
+++ MagicOcean/Droplet/DropletDetail.swift
@@ -0,0 +1,13 @@
+//
+//  DropletDetail.swift
+//  MagicOcean
+//
+//  Created by Wu Hengmin on 16/6/13.
+//  Copyright © 2016年 Wu Hengmin. All rights reserved.
+//
+
+import Cocoa
+
+class DropletDetail: UITableViewController {
+
+}
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-# MagicOcean
-=======
 # MagicOcean
 DigitalOcean第三方客户端
->>>>>>> 9d27b2c3bfd5402e02185eabdd500fefd239e0b1
