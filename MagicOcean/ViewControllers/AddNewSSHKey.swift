24a2e8ac60827fb986ea839dfb162bdb95e7bab1
月杪 <moonkinstar@gmail.com>
2016-06-20 15:04:14 +0800

    add new ssh key

--- MagicOcean/ViewControllers/AddNewSSHKey.swift
+++ MagicOcean/ViewControllers/AddNewSSHKey.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class AddNewSSHKey: UITableViewController, UITextFieldDelegate {
     
@@ -36,6 +37,35 @@ class AddNewSSHKey: UITableViewController, UITextFieldDelegate {
     }
     
     @IBAction func savePressed(sender: AnyObject) {
+//        curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer b7d03a6947b217efb6f3ec3bd3504582" -d '{"name":"My SSH Public Key","public_key":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAQQDDHr/jh2Jy4yALcK4JyWbVkPRaWmhck3IgCoeOO3z1e2dBowLh64QAM+Qb72pxekALga2oi4GvT+TlWNhzPH4V example"}' "https://api.digitalocean.com/v2/account/keys"
+        
+        let Headers = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer "+Account.sharedInstance.Access_Token
+        ]
+        
+        let parameters = [
+            "name":self.nameLabel.text,
+            "public_key":self.SSHKeyText.text
+        ]
+        
+        
+        weak var weakSelf = self
+        print(BASE_URL+URL_ACCOUNT+URL_KEYS)
+        Alamofire.request(.POST, BASE_URL+URL_ACCOUNT+"/"+URL_KEYS, parameters: parameters, encoding: .JSON, headers: Headers).responseJSON { response in
+            if let strongSelf = weakSelf {
+                let dic = response.result.value as! NSDictionary
+                print("response=\(dic)")
+                
+                if let message = dic.valueForKey("message") {
+                    print(message)
+                } else {
+                    strongSelf.dismissViewControllerAnimated(true, completion: {
+                        
+                    })
+                }
+            }
+        }
         
     }
     
--- MagicOcean/ViewControllers/Droplet/DropletDetail.swift
+++ MagicOcean/ViewControllers/Droplet/DropletDetail.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Alamofire
-import Toast_Swift
 import MBProgressHUD
 
 class DropletDetail: UITableViewController {
