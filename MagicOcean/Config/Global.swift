3763869b9dfc99f9bfe9a5daf703049fe3b45899
月杪 <moonkinstar@gmail.com>
2016-06-21 13:54:50 +0800

    remove config form git

--- MagicOcean/Config/Global.swift
+++ MagicOcean/Config/Global.swift
@@ -1,15 +1,10 @@
 //
-//  Config.swift
+//  Global.swift
 //  MagicOcean
 //
-//  Created by Wu Hengmin on 16/6/8.
+//  Created by Wu Hengmin on 16/6/21.
 //  Copyright © 2016年 Wu Hengmin. All rights reserved.
 //
-
-let ClientID = "60ef0ad0835c34bd06a314371027f58294b9b36e729777a4e898a22738c5a1c2"
-let ClientSecret = "586023a801f1523c42432ee86992c317d2bddd8d5b0cbbd3ed518134d75b3d22"
-let redirect_uri = "https://moonagic.com/callback"
-
 
 let BASE_URL = "https://api.digitalocean.com/v2/"
 let OAUTH_URL = "https://cloud.digitalocean.com/v1/"
@@ -24,7 +19,3 @@ let URL_IMAGES = "images"
 let URL_SIZES = "sizes"
 let URL_REGIONS = "regions"
 let URL_KEYS = "keys"
-
-
-
-
--- MagicOcean/Global.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Global.swift
-//  MagicOcean
-//
-//  Created by Wu Hengmin on 16/6/21.
-//  Copyright © 2016年 Wu Hengmin. All rights reserved.
-//
-
-import Foundation
